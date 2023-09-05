9843b7dd0fdeef2e792cabbb6b4c77ecf5f2f094
amgenious <henrytweneboah956@gmail.com>
2023-09-05 10:24:09 +0000

    shifted the close bar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,13 +6,13 @@ export const Navbar = () => {
   return (
     <div className='w-full h-16 bg-black flex fixed justify-between items-center p-2'>
         <div className='text-white'>
-        <div onClick={()=> setOpen(!open)} className={`text-2xl 2xl:hidden sm:visible text-box_color z-10 absolute ${open ? 'left-52':'left-2'} top-5`}>
+        <div onClick={()=> setOpen(!open)} className={`text-2xl 2xl:hidden sm:visible text-box_color z-10 absolute ${open ? 'left-64':'left-2'} top-5`}>
        <i className={open ? 'fa-solid fa-xmark':'fa-solid fa-bars'}></i> 
     </div>
     <div  className={`z-50 mt-2 2xl:block ${open ? 'flex':'hidden'}`}>
         <ol className='flex'>
-            <li className='mr-4'><Link to="/updateprofile">Update Profile</Link></li>
-            <li className='mr-4'><Link to="/home">Home</Link></li>
+            <li className='mr-2'><Link to="/updateprofile">Update Profile</Link></li>
+            <li className='mr-2'><Link to="/home">Home</Link></li>
             <li><a href="#">Logout</a></li>
         </ol>
     </div>
